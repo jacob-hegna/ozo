768b787422048b7805d7d14f9feda5bde0d7ea5e
Jacob Hegna <jacobhegna@gmail.com>
2021-09-24 23:42:53 -0500

    Fix config.cmake

--- cmake/ozo-config.cmake
+++ cmake/ozo-config.cmake
@@ -8,7 +8,7 @@ find_dependency(PostgreSQL)
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH_old_})
 
 find_dependency(Boost COMPONENTS coroutine context system thread atomic)
-find_dependency(resource_pool)
+find_dependency(resource-pool)
 
 include("${CMAKE_CURRENT_LIST_DIR}/ozo-targets.cmake")
 target_compile_options(yandex::ozo INTERFACE
